39a99bd09a579ce46da1b7287ed6ea2d11e69718
mzainulabidin70-cmyk <mzainulabidin70@gmail.com>
2026-09-14 22:24:02 -0900

    Enhance error handling in readJson function
    Added error handling for JSON parsing and file not found.

--- src/repositories/jsonRepository.js
+++ src/repositories/jsonRepository.js
@@ -1,12 +1,11 @@
 import fs from 'fs/promises';
 
-// Tugas 10 & 12: Membaca file JSON dengan Error Handling (Pengaman Eror)
 export async function readJson(filePath) {
   try {
     const data = await fs.readFile(filePath, 'utf-8');
     return JSON.parse(data);
   } catch (error) {
-    // Jalur merah: Jika gagal, periksa jenis erornya
+
     if (error.code === 'ENOENT') {
       console.error("File data tidak ditemukan");
     } else {
