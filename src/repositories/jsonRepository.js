e3431451c4893d3f40e9a24c68ebfc135dc66023
Zainul <mzainulabidin70@gmail.com>
2026-09-15 11:48:08 -0900

    feat: add async json writer

--- src/repositories/jsonRepository.js
+++ src/repositories/jsonRepository.js
@@ -4,3 +4,8 @@ export async function readJson(filePath) {
   const data = await fs.readFile(filePath, 'utf-8');
   return JSON.parse(data);
 }
+
+export async function writeJson(filePath, data) {
+  const jsonString = JSON.stringify(data, null, 2);
+  await fs.writeFile(filePath, jsonString, 'utf-8');
+}
